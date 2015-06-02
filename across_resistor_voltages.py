8cdb34eca7a6c8fa28aa543bd640e2ec3bf351c9
cmac4603 <colin@macrae.me>
2015-06-02 11:15:08 +0000

    post-testing on rasp_pi with demo and own programs successfully. website includes circuit diagrams used for this

--- across_resistor_voltages.py
+++ across_resistor_voltages.py
@@ -2,15 +2,15 @@
 
 from ABE_ADCPi import ADCPi
 from ABE_helpers import ABEHelpers
-import time
 import os
+import time
 
 i2c_helper = ABEHelpers()
 bus = i2c_helper.get_smbus()
 adc = ADCPi(bus, 0x68, 0x69, 12)
 
 v_r1 = adc.read_voltage(1)
-v_r2 = adc.read_voltage(2)
+#v_r2 = adc.read_voltage(2)
 
 while (True):
 
@@ -19,7 +19,7 @@ while (True):
 
     # read from adc channels and print to screen
     print ("Channel 1: %02f" % (v_r1))
-    print ("Channel 2: %02f" % (v_r2)) 
+    #print ("Channel 2: %02f" % (v_r2)) 
     
-    # wait 0.5 seconds before reading the pins again
-    time.sleep(0.5)
+    # wait 1 second before reading the pins again
+    time.sleep(1)
