9b63258b5e46bc87f95d0de4f7279047623d2f6e
cmac4603 <colin@macrae.me>
2015-06-04 14:28:26 +0000

    struggling with the resistance, but voltage and current should work

--- across_resistor_voltage_&_current.py
+++ across_resistor_voltage_&_current.py
@@ -9,10 +9,13 @@ i2c_helper = ABEHelpers()
 bus = i2c_helper.get_smbus()
 adc = ADCPi(bus, 0x68, 0x69, 12)
 
-v_r1 = adc.read_voltage(1)
+v1 = adc.read_voltage(1)
 
 def calcCurrent(inval):
-    return ((inval) - v_r1) / 0.066
+    return ((inval) - v1) / 0.066
+
+def calcResistance():
+    return v1 / calcCurrent(adc.read_voltage(2))
 
 while (True):
 
@@ -20,9 +23,9 @@ while (True):
     os.system('clear')
 
     # read from adc channels and print to screen
-    print ("Channel 1 voltage V: %02f" % v_r1)
-    print ("Channel 1 current I: %02f" % calcCurrent(adc.read_voltage(1)))
-    print ("Channel 1 resistance R: %02f" % v_r1/calcCurrent(adc.read_voltage(1))
+    print ("Channel 1 voltage V: %02f" % v1)
+    print ("Channel 1 current I: %02f" % calcCurrent(adc.read_voltage(2)))
+    print ("Channel 1 resistance R: %02f" % calcResistance())
 
     
     # wait 1 second before reading the pins again
