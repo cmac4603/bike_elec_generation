aa953c371c5bf194e1ad846903f949d7451ae8ba
cmac4603 <colin@macrae.me>
2015-07-15 14:12:30 +0000

    V,I, & R readings in v3

--- v_i_r {V2}.py
+++ v_i_r {V2}.py
@@ -14,10 +14,13 @@ v1 = adc.read_voltage(1)
 v2 = adc.read_voltage(2)
 
 def calcCurrent(inval):
-    return ((inval) - v1) / 0.066
+    return ((inval) - v1) / 0.066 as i1
 
-def calcResistance(v1, i1):
-    if i1 == 0:
+def calcResistance(volt, curr):
+    curr = calcCurrent(v2)
+    if volt == 0:
+        return 0
+    elif curr == 0:
         return 0
     else:
         return v1/i1
@@ -29,7 +32,7 @@ while (True):
 
     # read from adc channels and print to screen
     print ("Channel 1 voltage V: %02f" % v1)
-    print ("Channel 1 current I: %02f" % i1)
+    print ("Channel 1 current I: %02f" % calcCurrent(v2))
     print ("Channel 1 resistance R: %02f" % calcResistance(v1, i1))
 
     
